03e0e45e43b6bc8d13c851cf9b044f120303deb9
Moji00f <mojtabaahmadi2008@gmail.com>
2023-08-17 23:35:00 +0330

    Exercise Binding

--- src/api/handlers/health.go
+++ src/api/handlers/health.go
@@ -9,6 +9,16 @@ import (
 type HealthHandler struct {
 }
 
+type Header struct {
+	UserId  string
+	Browser string
+}
+
+type Person struct {
+	FirstName string
+	LastName  string
+}
+
 func NewHealthHandler() *HealthHandler {
 	return &HealthHandler{}
 }
@@ -16,5 +26,86 @@ func NewHealthHandler() *HealthHandler {
 func (h *HealthHandler) Health(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "working",
+	})
+}
+
+func (h *HealthHandler) HeaderBinder1(c *gin.Context) {
+	UserId := c.GetHeader("UserId")
+	c.JSON(http.StatusOK, gin.H{
+		"result": "HeaderBinder1",
+		"UserId": UserId,
+	})
+}
+
+func (h *HealthHandler) HeaderBinder2(c *gin.Context) {
+	header := Header{}
+	c.BindHeader(&header)
+	c.JSON(http.StatusOK, gin.H{
+		"result": "HeaderBinder2",
+		"header": header,
+	})
+}
+
+func (h *HealthHandler) QueryBinder1(c *gin.Context) {
+	id := c.Query("id")
+	name := c.Query("name")
+	c.JSON(http.StatusOK, gin.H{
+		"result": "QueryBinder1",
+		"Id":     id,
+		"Name":   name,
+	})
+}
+
+func (h *HealthHandler) QueryBinder2(c *gin.Context) {
+	ids := c.QueryArray("id")
+	name := c.Query("name")
+	c.JSON(http.StatusOK, gin.H{
+		"result": "QueryBInder2",
+		"Ids":    ids,
+		"name":   name,
+	})
+}
+
+func (h *HealthHandler) UriBinder(c *gin.Context) {
+	id := c.Param("id")
+	name := c.Param("name")
+	c.JSON(http.StatusOK, gin.H{
+		"result": "UriBinder",
+		"id":     id,
+		"name":   name,
+	})
+}
+
+func (h *HealthHandler) BodyBinder(c *gin.Context) {
+	p := Person{}
+	c.ShouldBindJSON(&p)
+	c.JSON(http.StatusOK, gin.H{
+		"result": "BodyBinder",
+		"person": p,
+	})
+}
+
+func (h *HealthHandler) FormBinder(c *gin.Context) {
+	p := Person{}
+	c.ShouldBindJSON(&p)
+	c.JSON(http.StatusOK, gin.H{
+		"result": "BodyBinder",
+		"person": p,
+	})
+}
+
+func (h *HealthHandler) FileBinder(c *gin.Context) {
+	file, _ := c.FormFile("file")
+	err := c.SaveUploadedFile(file, "file")
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"resutl":   "FileBinder",
+		"FileName": file.Filename,
 	})
 }
--- src/api/routes/health.go
+++ src/api/routes/health.go
@@ -9,4 +9,16 @@ func Health(r *gin.RouterGroup) {
 	health := handlers.NewHealthHandler()
 
 	r.GET("/", health.Health)
+
+	r.POST("/binder/header1", health.HeaderBinder1)
+	r.POST("/binder/header2", health.HeaderBinder2)
+
+	r.POST("/binder/query1", health.QueryBinder1)
+	r.POST("/binder/query2", health.QueryBinder2)
+
+	r.POST("/binder/uri/:id/:name", health.UriBinder)
+
+	r.POST("/binder/body", health.BodyBinder)
+	r.POST("/binder/form", health.FormBinder)
+	r.POST("/binder/file", health.FileBinder)
 }
