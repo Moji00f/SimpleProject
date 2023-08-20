42a10d89a08aad0d5f0f36462ac5c3a55dceced8
Moji00f <mojtabaahmadi2008@gmail.com>
2023-08-20 17:08:42 +0330

    add logger feature

--- src/cmd/maing.go
+++ src/cmd/maing.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"log"
-
 	"github.com/Moji00f/SimpleProject/api"
 	"github.com/Moji00f/SimpleProject/config"
 	"github.com/Moji00f/SimpleProject/data/cache"
 	"github.com/Moji00f/SimpleProject/data/db"
+	"github.com/Moji00f/SimpleProject/pkg/logging"
 )
 
 // @securityDefinitions.apikey AuthBearer
@@ -14,16 +13,17 @@ import (
 // @name Authorization
 func main() {
 	cfg := config.GetConfig()
+	logger := logging.NewLogger(cfg)
 	err := cache.InitRedis(cfg)
 	defer cache.CloseRedis()
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(logging.Redis, logging.Startup, err.Error(), nil)
 	}
 
 	err = db.InitDb(cfg)
 	defer db.CloseDb()
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(logging.Postgres, logging.Startup, err.Error(), nil)
 	}
 
 	api.InitServer(cfg)
--- src/common/persian.go
+++ src/common/persian.go
@@ -1,9 +1,13 @@
 package common
 
 import (
-	"log"
 	"regexp"
+
+	"github.com/Moji00f/SimpleProject/config"
+	"github.com/Moji00f/SimpleProject/pkg/logging"
 )
+
+var logger = logging.NewLogger(config.GetConfig())
 
 const iranianMobileNumberPattern string = `^09(1[0-9]|2[0-2]|3[0-9]|9[0-9])[0-9]{7}$`
 
@@ -11,7 +15,7 @@ func IranianMobileNumberValidator(mobileNumber string) bool {
 
 	res, err := regexp.MatchString(iranianMobileNumberPattern, mobileNumber)
 	if err != nil {
-		log.Print(err.Error())
+		logger.Error(logging.Validation, logging.MobileValidation, err.Error(), nil)
 	}
 
 	return res
