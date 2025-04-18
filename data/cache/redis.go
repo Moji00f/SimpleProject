d1e2908880d593c2930f5de77715ad867d210a78
Moji00f <mojtabaahmadi2008@gmail.com>
2025-04-18 18:22:35 +0330

    add set and get function to redis package

--- data/cache/redis.go
+++ data/cache/redis.go
@@ -2,6 +2,7 @@ package cache
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/Moji00f/SimpleProject/config"
 	"github.com/go-redis/redis/v8"
@@ -39,4 +40,30 @@ func GetRedis() *redis.Client {
 
 func CloseRedis() {
 	redisClient.Close()
+}
+
+func Set[T any](c *redis.Client, key string, value T, duration time.Duration) error {
+	v, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+
+	return c.Set(context.Background(), key, v, duration).Err()
+}
+
+func Get[T any](c *redis.Client, key string) (T, error) {
+	//var dest *T = new(T)
+	var dest T = *new(T)
+
+	v, err := c.Get(context.Background(), key).Result()
+	if err != nil {
+		return dest, err
+	}
+
+	err = json.Unmarshal([]byte(v), &dest)
+	if err != nil {
+		return dest, err
+	}
+
+	return dest, nil
 }
